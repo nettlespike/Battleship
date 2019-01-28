6b8981de0a67c084f6ff8ea1b05ab9d95ae49f4a
Soup <39444003+magicalsoup@users.noreply.github.com>
2019-01-27 23:17:47 -0500

    Create Sunken.java

---
+++ Sunken.java
@@ -0,0 +1,13 @@
+package Battleship;
+
+class Sunken{
+	int s; //ship type
+	int x, y; //coordinates 
+	String type;
+	public Sunken(int s, int x, int y, String type) {
+		this.s = s;
+		this.x = x;
+		this.y = y;
+		this.type = type;
+	}
+}
